5505278d624356ec572c1e25c0e8904a0d8e24da
Maciej Pfutzner <maciej.pfutzner.12@ucl.ac.uk>
2017-06-06 22:47:00 +0100

    Hack around bitcoin timeouts
    Make bitcoin_price return latest price if waiting too long instead of
    just waiting forever. Also reduce the max time waiting.

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -2,18 +2,26 @@ import requests
 
 API = "http://api.coindesk.com/v1/bpi/currentprice.json"
 
+last_price = 0
 # TODO: Try to catch timeout exceptions?...
 def get_bpi_info():
-    while True:
-        req = requests.get(API)
+    try:
+        req = requests.get(API, timeout=5)
         if req.status_code == 200:
             return req.json()
+        else:
+            return {"bpi":{"USD": {"rate_float": str(last_price)}}}
+    except requests.exceptions.ReadTimeout:
+        return {"bpi":{"USD": {"rate_float": str(last_price)}}}
+
 
 # Get Bitcoin rate in currency
 def get_rate(currency="usd"):
     currency = currency.upper()
     bpi_info = get_bpi_info()
-    return bpi_info['bpi'][currency]["rate_float"]
+    global last_price
+    last_price = bpi_info['bpi'][currency]["rate_float"]
+    return last_price
 
 
 if __name__ == '__main__':
